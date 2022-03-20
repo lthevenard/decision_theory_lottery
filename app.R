6cff70684fab1d441785cbcad6d1a90c85a6ed7a
Lucas Thevenard Gomes <lthevenard@gmail.com>
2022-03-20 20:18:01 -0300

    Alpha version of the app.

---
+++ app.R
@@ -0,0 +1,173 @@
+library(shiny)
+library(scales)
+library(bslib)
+library(tidyverse)
+library(plotly)
+library(DT)
+source("resources.R")
+
+
+ui <- fluidPage(
+    theme = bs_theme(bootswatch = "sandstone"),
+    titlePanel("Expected value of a lottery: a simple simulation"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            h3("Lottery 1"),
+            textInput("values_1", "Payoffs", value = "20; 40; 0"),
+            textInput("percentages_1", "Percentages", value = "0.25; 0.25; 0.5"),
+            h3("Lottery 2"),
+            textInput("values_2", "Payoffs", value = "120; 140; -100"),
+            textInput("percentages_2", "Percentages", value = "0.25; 0.25; 0.5"),
+            h3("Simulations"),
+            sliderInput("simulation_n", "Number of simulations", min = 100, max = 1000, value = 500, step = 50),
+            actionButton("go","Simulate")
+        ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Lottery 1",
+                         htmlOutput("disclaimer_1"),
+                         htmlOutput("lottery_description_1"),
+                         plotlyOutput("theoretical_dist_plot_1"),
+                         htmlOutput("lottery_summary_1"),
+                         plotlyOutput("expected_value_plot_1"),
+                         htmlOutput("table_1_header"),
+                         dataTableOutput("simulation_table_1"),
+                         br(), br()),
+                tabPanel("Lottery 2",
+                         htmlOutput("disclaimer_2"),
+                         htmlOutput("lottery_description_2"),
+                         plotlyOutput("theoretical_dist_plot_2"),
+                         htmlOutput("lottery_summary_2"),
+                         plotlyOutput("expected_value_plot_2"),
+                         htmlOutput("table_2_header"),
+                         dataTableOutput("simulation_table_2"),
+                         br(), br()),
+                tabPanel("Simulation Comparisons",
+                         br(), 
+                         sliderInput("tickets_n", "Number of tickets (N)", min = 1, max = 1000, 
+                                     value = 10, step = 10, animate = TRUE), 
+                         br(),
+                         plotlyOutput("percentage_distribution_plot"),
+                         br(),
+                         plotlyOutput("mean_returns_plot_1"),
+                         br(),
+                         plotlyOutput("mean_returns_plot_2"),
+                         br()),
+                tabPanel("Instructions",
+                         htmlOutput("instructions"))
+            )
+        )
+    )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+    output$instructions <- renderUI({HTML(instructions)})
+    output$disclaimer_1 <- renderUI({HTML(disclaimer)})
+    output$disclaimer_2 <- renderUI({HTML(disclaimer)})
+    # Lottery 1
+    describe_1 <- eventReactive(input$go,{describe_lottery(input$values_1, input$percentages_1)})
+    output$lottery_description_1 <- renderUI({HTML(describe_1()$descriptions)})
+    output$theoretical_dist_plot_1 <- renderPlotly({
+        describe_1() %>% 
+            plot_theoretical_distribution()
+    })
+    output$lottery_summary_1 <- renderUI({
+        HTML(describe_1() %>% summarise_lottery())
+    })
+    output$expected_value_plot_1 <- renderPlotly({
+        describe_1() %>% 
+            plot_expected_value()
+    })
+    table_1_header <- eventReactive(input$go,{
+        h2("Simulation Table - Lottery 1")
+    })
+    output$table_1_header <- renderUI({
+        table_1_header()
+    })
+    get_sim_table_1_react <- reactive({
+        description_list <- describe_1()
+        get_sim_table(input$simulation_n, 
+                      description_list$cum_percentages, 
+                      description_list$result_names, 
+                      description_list$values, 
+                      description_list$expected_value)
+    })
+    output$simulation_table_1 <- renderDataTable({
+        get_sim_table_1_react()  %>% 
+            datatable(rownames= FALSE) %>% 
+            formatStyle(
+                columns = c("Returns", "Mean Returns", "Profit"), 
+                color = styleInterval(cuts = -0.0001, values = c("red", "green"))
+            )
+    })
+    # Lottery 2
+    describe_2 <- eventReactive(input$go,{describe_lottery(input$values_2, input$percentages_2)})
+    output$lottery_description_2 <- renderUI({HTML(describe_2()$descriptions)})
+    output$theoretical_dist_plot_2 <- renderPlotly({
+        describe_2() %>% 
+            plot_theoretical_distribution()
+    })
+    output$lottery_summary_2 <- renderUI({
+        HTML(describe_2() %>% summarise_lottery())
+    })
+    output$expected_value_plot_2 <- renderPlotly({
+        describe_2() %>% 
+            plot_expected_value()
+    })
+    table_2_header <- eventReactive(input$go,{
+        h2("Simulation Table - Lottery 2")
+    })
+    output$table_2_header <- renderUI({
+        table_2_header()
+    })
+    get_sim_table_2_react <- reactive({
+        description_list <- describe_2()
+        get_sim_table(input$simulation_n, 
+                      description_list$cum_percentages, 
+                      description_list$result_names, 
+                      description_list$values, 
+                      description_list$expected_value)
+    })
+    output$simulation_table_2 <- renderDataTable({
+        get_sim_table_2_react() %>%
+            datatable(rownames= FALSE) %>% 
+            formatStyle(
+                columns = c("Returns", "Mean Returns", "Profit"), 
+                color = styleInterval(cuts = -0.0001, values = c("red", "green"))
+            )
+    })
+    # Simulation slider
+    observeEvent({input$simulation_n},{
+            updateSliderInput(session,"tickets_n", max = input$simulation_n)
+        })
+    output$percentage_distribution_plot <- renderPlotly({
+        tickets <- input$tickets_n
+        table_1 <- get_sim_table_1_react() %>%
+            filter(Tickets == tickets) %>% 
+            select(!all_of(c("Tickets", "Returns", "Mean Returns", "Profit")))
+        table_2 <- get_sim_table_2_react() %>%
+            filter(Tickets == tickets) %>% 
+            select(!all_of(c("Tickets", "Returns", "Mean Returns", "Profit")))
+        plot_percentage_comparison(table_1, table_2, tickets)
+    })
+    output$mean_returns_plot_1 <- renderPlotly({
+        get_sim_table_1_react() %>% 
+            plot_dot_mean_returns("Lottery 1", describe_1()$expected_value)
+    })
+    output$mean_returns_plot_2 <- renderPlotly({
+        get_sim_table_2_react() %>% 
+            plot_dot_mean_returns("Lottery 2", describe_2()$expected_value)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
---
+++ resources.R
@@ -0,0 +1,232 @@
+# FUNCTIONS ----
+
+## Lottery description ----
+
+get_result_names <- function(n) {
+  alphabet <- str_split("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z", ",") %>% 
+    unlist()
+  return(alphabet[1:n])
+}
+
+calc_expected_value <- function(values, percentages) {
+  return(sum(values * percentages))
+}
+
+segment_number_string <- function(number_string) {
+  return(
+    number_string %>% 
+      str_split(";") %>% 
+      unlist() %>% 
+      str_trim() %>% 
+      as.numeric()
+  )
+}
+
+test_inputs <- function(values, percentages) {
+  return((
+    class(values) == "try-error" || class(percentages) == "try-error" || any(is.na(values)) || any(is.na(percentages)) || length(values) != length(percentages) || sum(percentages) != 1 
+  ))
+}
+
+bold <- function(string, color = NULL) {
+  if (is.null(color)) {
+    return(paste0("<b>", string, "</b>"))
+  } else {
+    return(paste0("<b style='color:", color, ";'>", string, "</b>"))
+  }
+  
+}
+
+describe_option <- function(result_name, value, percentage) {
+  return(paste("<li>Outcome", bold(result_name), 
+               "pays", ifelse(value >= 0, bold(value, color = "green"), bold(value, color = "red")), 
+               "with a", bold(paste0(round(percentage*100), "%")), "chance</li>")
+  )
+}
+
+description_header <- function(description) {
+  paste0(
+    h2("Lotery returns"), "<br><ul>", paste(description, collapse=""), "</ul><br><br>"
+  )
+}
+
+cumulate_percentages <- function(percentages) {
+  n <- 1:length(percentages)
+  cum_percentages <- map_dbl(n, ~sum(percentages[1:.]))
+  return(cum_percentages)
+}
+
+output_description <- function(description_list) {
+  if (description_list$result_names[[1]] == "error") {
+    return("<p style='color:red;'>The inputs were not provided correctly. Please follow the instructions.</p>")
+  } else {
+    return(description_list$descriptions)
+  }
+}
+
+theoretical_deviation <- function(theoretical_distribution) {
+  values <- as.numeric(theoretical_distribution$Values)
+  frequencies <- theoretical_distribution$Frequency
+  dist <- map2(values, frequencies, rep) %>% unlist()
+  return(sd(dist))
+}
+
+describe_lottery <- function(values_string, percentages_string) {
+  values <- try(segment_number_string(values_string), silent = TRUE)
+  percentages <- try(segment_number_string(percentages_string), silent = TRUE)
+  problem_with_inputs <- test_inputs(values, percentages)
+  if (problem_with_inputs) {
+    return(list(
+      descriptions = "<br><p style = 'color:red;'><b>Incorrect input. Please try again.</b></p>"
+      )
+    )
+  } else {
+    n <- length(values)
+    cum_percentages <- cumulate_percentages(percentages)
+    expected_value <- calc_expected_value(values, percentages)
+    result_names <- get_result_names(n)
+    descriptions <- map_chr(1:n, ~describe_option(result_names[.], values[.], percentages[.])) %>% 
+      description_header()
+    theoretical_distribution <- tibble(Values = fct_infreq(factor(values)), Frequency = round(percentages*100))
+    return(list(
+      result_names = result_names,
+      values = values,
+      percentages = percentages,
+      cum_percentages = cum_percentages,
+      expected_value = expected_value,
+      descriptions = descriptions,
+      theoretical_distribution = theoretical_distribution,
+      theoretical_deviation = theoretical_deviation(theoretical_distribution)
+    ))
+  }
+}
+
+summarise_lottery <- function(description_list) {
+  header <- h2("Solution")
+  expected_value <- paste0(
+    "<li style = 'color:#1e2938'><b>Expected value: ", as.character(
+      round(description_list$expected_value, digits=2)
+    ), "</b></li>"
+  )
+  theoretical_variance <- paste0(
+    "<li style = 'color:gray'>Theoretical standard deviation: ", as.character(
+      round(theoretical_deviation(description_list$theoretical_distribution), digits=2)
+    ), "</li>"
+  )
+  return(paste0(
+    header, "<br>", "<ul style='margin-left=8px; margin-bottom=8px;'>",
+    expected_value, theoretical_variance, "</ul><br>"
+  ))
+}
+
+## Lottery simulation ----
+
+
+select_result <- function(value, cum_percentages, result_names) {
+  return(result_names[min(which(cum_percentages >= value))])
+}
+
+simulate_n_tickets <- function(n, cum_percentages, result_names) {
+  dist <- runif(n)
+  return(
+    map_chr(dist, ~select_result(., cum_percentages, result_names))
+  )
+}
+
+n_tickets_register <- function(simulation, result_names, values) {
+  output <- list()
+  output[["Tickets"]] <- length(simulation)
+  total_value <- 0
+  for (i in seq_along(result_names)) {
+    output[[result_names[[i]]]] <- length(simulation[simulation == result_names[[i]]])
+    total_value <- total_value + (output[[result_names[[i]]]] * values[[i]])
+  }
+  output[["Returns"]] <- total_value
+  return(bind_cols(output))
+}
+
+get_sim_table <- function(n, cum_percentages, result_names, values, expected_value) {
+  tickets <- 1:n
+  sims <- map(tickets, ~simulate_n_tickets(., cum_percentages, result_names))
+  sim_table <- map_dfr(sims, ~n_tickets_register(., result_names, values)) %>% 
+    mutate(`Mean Returns` = round(Returns / Tickets, digits = 2), 
+           Profit = round((`Mean Returns` - expected_value) * Tickets, digits = 2))
+  return(sim_table)
+}
+
+# Ploting functions ----
+
+plot_theoretical_distribution <- function(description_list) {
+  description_list$theoretical_distribution %>% 
+    ggplot(aes(x = Values, y = Frequency)) +
+    geom_col(fill = "#344e71", color = "#1e2938", width = 0.95) +
+    labs(title = "Theoretical Distribution of Returns (100 Tickets)") +
+    theme_bw()
+}
+
+plot_expected_value <- function(description_list) {
+  expected_value <- description_list$expected_value
+  deviation <- expected_value * description_list$theoretical_deviation
+  value_min <- expected_value - (2 * deviation)
+  value_low <- expected_value - deviation
+  value_high <- expected_value + deviation
+  value_max <- expected_value + (2 * deviation)
+  tibble(
+    object = "Lottery",
+    expected_value = expected_value,
+    deviation = deviation
+  ) %>% 
+    ggplot(aes(x = object, y = expected_value, 
+               ymin = expected_value - deviation,
+               ymax = expected_value + deviation)) +
+    geom_errorbar(color = "#1e2938", width = 0.5) +
+    geom_point(color = "#1e2938", size = 3) +
+    scale_y_continuous(breaks = c(value_min, value_low, expected_value, value_high, value_max),
+                       limits = c(value_min, value_max)) +
+    labs(title = "Expected value and deviation",
+         x = "", y = "") +
+    theme_bw()
+}
+
+plot_percentage_comparison <- function(table_1, table_2, tickets) {
+  table <- bind_rows(
+    pivot_longer(table_1, names(table_1), names_to = "Outcome") %>% 
+      mutate(Frequency = value / tickets,
+             Lottery = "Lottery 1"),
+    pivot_longer(table_2, names(table_2), names_to = "Outcome") %>% 
+      mutate(Frequency = value / tickets,
+             Lottery = "Lottery 2")
+  )
+  table %>% 
+    ggplot(aes(x = Outcome, y = Frequency)) +
+    geom_col(fill = "#344e71", color = "#1e2938", width = 0.95) +
+    scale_y_continuous(labels = label_percent(accuracy = 1),
+                       limits = c(0, 1), breaks = c(0, 0.25, 0.5, 0.75, 1)) +
+    labs(title = paste0(tickets, " Ticket", ifelse(tickets == 1, "", "s"), ": Relative Frequency of Outcomes"), 
+         y = "Percentage Frequency\n\n") +
+    theme_bw() +
+    facet_wrap(~Lottery, scales = "free_x")
+}
+
+plot_dot_mean_returns <- function(table, lottery, expected_value) {
+  table %>% 
+    ggplot(aes(x = Tickets, y = `Mean Returns`)) +
+    geom_point(color = "#344e71", size = 1, alpha = 0.7) +
+    geom_hline(yintercept = expected_value, linetype = "dashed", color = "red") +
+    labs(title = paste0("Mean Returns of ", lottery, " (simulation results)"),
+         x = "Number of Tickets") +
+    theme_bw()
+}
+
+# OTHER VARIABLES ----
+
+instructions <- paste0(h3("Instructions"),
+                       "<ul style = 'margin-left:8px;'><li style = 'margin-bottom:8px;'>Payoffs and percentages should be provided as lists of numbers separated by semicolons (;);</li>",
+                       "<li style = 'margin-bottom:8px;'>Payoffs can be positive or negative numbers, to indicate a negative number simply use the minus sign (-);</li>",
+                       "<li style = 'margin-bottom:8px;'>Percentages should be decimal numbers between 0 and 1;</li>",
+                       "<li style = 'margin-bottom:8px;'>Use the American convention for decimal numbers: use a dot (.), not a comma (,);</li>",
+                       "<li style = 'margin-bottom:8px;'>The sum of all percentages must equal 1 (100%);</li>",
+                       "<li style = 'margin-bottom:8px;'>The length of both lists must be equal and the maximum length is 20.</li></ul><br><br>")
+
+disclaimer <- "<br><i>Please provide the information about the lotteries you want to simulate according to the instructions, then press 'Simulate'.</i><br>"
+
